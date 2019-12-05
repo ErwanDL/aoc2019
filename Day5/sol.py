fb3aaa9f158dc28ef7074be145ccbb8e5f58497d
ErwanDL <erwan.de-lepinau@student.ecp.fr>
2019-12-05 19:43:28 +0100

    refactored solution for Day5 using a class

--- Day5/sol.py
+++ Day5/sol.py
@@ -1,11 +1,7 @@
 import os
 dirname = os.path.dirname(__file__)
-
 input_txt = open(dirname + "/input.txt", "r").read()
-
 input_list = list(map(int, input_txt.split(",")))
-
-data = input_list[:]
 
 
 def get_digit_right_to_left(number, digit_position):
@@ -19,115 +15,94 @@ def get_digit_right_to_left(number, digit_position):
         return get_digit_right_to_left(number // 10, digit_position - 1)
 
 
-# Every operation returns an instruction pointer value
+class IntcodeParser():
+    def __init__(self, data):
+        self.data = data
+        self.pointer = 0
+        self.modes = 0
+
+    def param(self, param_nb):
+        """
+            Applies the appropriate mode to the param at self.pointer + param_nb
+            and returns the resulting value.
+        """
+        mode = get_digit_right_to_left(self.modes, param_nb - 1)
+        param_index = self.pointer + param_nb
+        return self.data[param_index] if mode == 1 else self.data[
+            self.data[param_index]]
+
+    def op_sum(self):
+        storage_index = self.data[self.pointer + 3]
+        self.data[storage_index] = self.param(1) + self.param(2)
+        self.pointer += 4
+
+    def op_multiply(self):
+        storage_index = self.data[self.pointer + 3]
+        self.data[storage_index] = self.param(1) * self.param(2)
+        self.pointer += 4
+
+    def op_input(self):
+        value = input("Enter the desired value : ")
+        storage_index = self.data[self.pointer + 1]
+        self.data[storage_index] = int(value)
+        self.pointer += 2
+
+    def op_output(self):
+        print(self.param(1))
+        self.pointer += 2
+
+    def op_jump_if_true(self):
+        self.pointer = self.param(2) if (
+            self.param(1) != 0) else self.pointer + 3
+
+    def op_jump_if_false(self):
+        self.pointer = self.param(2) if (
+            self.param(1) == 0) else self.pointer + 3
+
+    def op_less_than(self):
+        storage_index = self.data[self.pointer + 3]
+        self.data[storage_index] = 1 if (self.param(1) < self.param(2)) else 0
+        self.pointer += 4
+
+    def op_equal_to(self):
+        storage_index = self.data[self.pointer + 3]
+        self.data[storage_index] = 1 if (self.param(1) == self.param(2)) else 0
+        self.pointer += 4
+
+    def parse_next_instruction(self):
+        """
+            Parses an instruction, executes it with the correct mode and returns
+            the index of the next instruction or -1 if terminated.
+        """
+        instruction = self.data[self.pointer]
+        opcode = instruction % 100
+
+        if opcode == 99:
+            self.pointer = -1
+
+        self.modes = instruction // 100
+
+        if opcode == 1:
+            self.op_sum()
+        if opcode == 2:
+            self.op_multiply()
+        if opcode == 3:
+            self.op_input()
+        if opcode == 4:
+            self.op_output()
+        if opcode == 5:
+            self.op_jump_if_true()
+        if opcode == 6:
+            self.op_jump_if_false()
+        if opcode == 7:
+            self.op_less_than()
+        if opcode == 8:
+            self.op_equal_to()
+
+    def run(self):
+        while self.pointer != -1:
+            self.parse_next_instruction()
 
 
-def op_sum(instruction_index, i_first_value, i_second_value, i_store_result):
-    data[i_store_result] = data[i_first_value] + data[i_second_value]
-    return instruction_index + 4
-
-
-def op_multiply(instruction_index, i_first_value, i_second_value,
-                i_store_result):
-    data[i_store_result] = data[i_first_value] * data[i_second_value]
-    return instruction_index + 4
-
-
-def op_input(instruction_index, i_store_input):
-    value = input("Enter the desired value : ")
-    data[i_store_input] = int(value)
-    return instruction_index + 2
-
-
-def op_output(instruction_index, i_value):
-    print(data[i_value])
-    return instruction_index + 2
-
-
-def op_jump_if_true(instruction_index, i_first_value, i_second_value):
-    if data[i_first_value] != 0:
-        return data[i_second_value]
-    else:
-        return instruction_index + 3
-
-
-def op_jump_if_false(instruction_index, i_first_value, i_second_value):
-    if data[i_first_value] == 0:
-        return data[i_second_value]
-    else:
-        return instruction_index + 3
-
-
-def op_less_than(instruction_index, i_first_value, i_second_value,
-                 i_store_result):
-    if data[i_first_value] < data[i_second_value]:
-        data[i_store_result] = 1
-    else:
-        data[i_store_result] = 0
-    return instruction_index + 4
-
-
-def op_equal_to(instruction_index, i_first_value, i_second_value,
-                i_store_result):
-    if data[i_first_value] == data[i_second_value]:
-        data[i_store_result] = 1
-    else:
-        data[i_store_result] = 0
-    return instruction_index + 4
-
-
-def apply_mode_to_param(modes, instruction_index, param_nb):
-    """
-        Applies the corresponding mode to the input param_nb based on the
-        "modes" of the associated instruction.
-        Returns an index (the parameter's index if
-        immediate mode, or its value if position mode).
-    """
-    mode = get_digit_right_to_left(modes, param_nb - 1)
-    if mode == 1:
-        return instruction_index + param_nb
-    else:
-        return data[instruction_index + param_nb]
-
-
-def parse_instruction(index):
-    """
-        Parses an instruction, executes it with the correct mode and returns
-        the index of the next instruction or -1 if terminated.
-    """
-    instruction = data[index]
-    opcode = instruction % 100
-
-    if opcode == 99:
-        return -1
-
-    modes = instruction // 100
-
-    def param(param_nb):
-        return apply_mode_to_param(modes, index, param_nb)
-
-    if opcode == 1:
-        return op_sum(index, param(1), param(2), param(3))
-    if opcode == 2:
-        return op_multiply(index, param(1), param(2), param(3))
-    if opcode == 3:
-        return op_input(index, param(1))
-    if opcode == 4:
-        return op_output(index, param(1))
-    if opcode == 5:
-        return op_jump_if_true(index, param(1), param(2))
-    if opcode == 6:
-        return op_jump_if_false(index, param(1), param(2))
-    if opcode == 7:
-        return op_less_than(index, param(1), param(2), param(3))
-    if opcode == 8:
-        return op_equal_to(index, param(1), param(2), param(3))
-
-
-def run_program():
-    index = 0
-    while index != -1:
-        index = parse_instruction(index)
-
-
-run_program()
+parser = IntcodeParser(input_list[:])
+parser.run()
